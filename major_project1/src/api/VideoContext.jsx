db34501071d4ca9d4fc7806445af8cb2611e26a3
Piyush-daryapurkar <piyushdaryapurkar309@gmail.com>
2025-02-17 23:19:20 +0530

    add data

--- major_project1/src/api/VideoContext.jsx
+++ major_project1/src/api/VideoContext.jsx
@@ -46,6 +46,14 @@ export const VideoProvider = ({ children }) => {
       thumbnail: "sslider6.webp",
       url: "https://www.w3schools.com/html/mov_bbb.mp4",
     },
+    {
+      id: 7,
+      title: "Comedy Show",
+      category: "Home",
+      thumbnail: "sslider6.webp",
+      url: "https://www.w3schools.com/html/mov_bbb.mp4",
+    },
+
 
 
   ]);
